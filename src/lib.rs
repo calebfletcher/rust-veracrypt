d2822f9c3fad1139b559eb195706a3eed0874abd
Caleb Fletcher <caleb@fletcher.cf>
2022-05-26 00:08:53 +1000

    feat: add header decode

--- src/lib.rs
+++ src/lib.rs
@@ -18,17 +18,25 @@ pub enum Error {
     InvalidVolume,
     #[error("invalid key")]
     InvalidKey,
+    #[error("invalid header: {0}")]
+    InvalidHeader(binrw::Error),
 }
 
 pub struct Volume<D: io::Read + io::Seek> {
     data: D,
+    header: Option<VolumeHeader>,
+    xts: Option<Xts128<Aes256>>,
 }
 
 impl Volume<File> {
     pub fn open<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
         let file = File::open(path).map_err(Error::FileOpenFailure)?;
 
-        Ok(Self { data: file })
+        Ok(Self {
+            data: file,
+            header: None,
+            xts: None,
+        })
     }
 }
 
@@ -51,7 +59,7 @@ impl<D: io::Read + io::Seek> Volume<D> {
         let mut key = [0; 64];
         pbkdf2::pbkdf2::<hmac::Hmac<sha2::Sha512>>(password.as_bytes(), salt, rounds, &mut key);
 
-        // Setup AES XTS decryption
+        // Setup header decryption
         let cipher_1 = Aes256::new(GenericArray::from_slice(&key[..32]));
         let cipher_2 = Aes256::new(GenericArray::from_slice(&key[32..]));
         let xts = Xts128::<Aes256>::new(cipher_1, cipher_2);
@@ -74,14 +82,48 @@ impl<D: io::Read + io::Seek> Volume<D> {
             return Err(Error::InvalidKey);
         }
 
+        // Decode header
+        let header =
+            VolumeHeader::read(&mut io::Cursor::new(header)).map_err(Error::InvalidHeader)?;
+
+        // Set up data decryption
+        let cipher_1 = Aes256::new(GenericArray::from_slice(&header.master_keys[..32]));
+        let cipher_2 = Aes256::new(GenericArray::from_slice(&header.master_keys[32..]));
+        let xts = Xts128::<Aes256>::new(cipher_1, cipher_2);
+
+        //xts.decrypt_area([64..], 448, 0, get_tweak_default);
+
+        // Move to the start of the data area
+        self.data
+            .seek(io::SeekFrom::Start(header.master_key_scope_offset))
+            .map_err(|_| Error::InvalidVolume)?;
+
+        self.header.replace(header);
+        self.xts.replace(xts);
+
         Ok(())
     }
 }
 
+#[allow(dead_code)]
 #[derive(Debug, BinRead)]
-pub struct VolumeFormat {
-    _salt: [u8; 64],
-    _offset: binrw::PosValue<()>,
+#[br(big)]
+pub struct VolumeHeader {
+    salt: [u8; 64],
+    #[br(magic = b"VERA")]
+    version: u16,
+    minimum_program_version: u16,
+    master_key_crc: u32,
+    #[br(pad_before = 16)]
+    hidden_volume_size: u64,
+    volume_size: u64,
+    master_key_scope_offset: u64,
+    master_key_scope_size: u64,
+    flags: u32,
+    sector_size: u32,
+    #[br(pad_before = 120)]
+    header_checksum: u32,
+    master_keys: [u8; 64], // NOTE: this assumes 2x256 bit keys (i.e. AES-256 mode)
 }
 
 #[cfg(test)]
